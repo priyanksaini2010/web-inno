71cebfff57c02adfe6a97ada183eeca1bed973c3
Priyank Saini <priyank.saini@fisglobal.com>
2021-05-26 09:58:15 +0530

    Initial Commit

---
+++ subscribe.php
@@ -0,0 +1,120 @@
+<!doctype html>
+                        <html>
+                            <head>
+                                <meta charset='utf-8'>
+                                <meta name='viewport' content='width=device-width, initial-scale=1'>
+                                <title>Snippet - BBBootstrap</title>
+                                <link href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css' rel='stylesheet'>
+                                <link href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css' rel='stylesheet'>
+                                <style>body {
+    background-color: #eeeeee
+}
+
+.green {
+    color: rgb(15, 207, 143);
+    font-weight: 680
+}
+
+@media(max-width:567px) {
+    .mobile {
+        padding-top: 40px
+    }
+}</style>
+                                <script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+                                <script type='text/javascript' src='https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js'></script>
+                                <script type='text/javascript'></script>
+                            </head>
+                            <body oncontextmenu='return false' class='snippet-body'>
+                            <div class="container rounded bg-white">
+    <div class="row d-flex justify-content-center pb-5">
+        <div class="col-sm-5 col-md-5 ml-1">
+            <div class="py-4 d-flex flex-row">
+                <h5><img height="20px" src="FIS.png"/>&nbsp;<b>Webpay</b> | </h5><span class="pl-2">Payment Gateway</span>
+            </div>
+            <h4 class="green">$399.00</h4>
+            <h4>PREMIUM</h4>
+            <div class="d-flex pt-2">
+                <div>
+                    <p><b>1 meal every day @ </b> <span class="green">$399 Per Month</span></p>
+                </div>
+               
+            </div>
+            <hr>
+            <div class="pt-2">
+                <form class="pb-3" accept="pay.php" method="post">
+                    <div class="d-flex flex-row align-content-center">
+                        <div class="pt-2 pr-2"><input type="radio" name="radio1" id="r1" checked></div>
+                        <div class="rounded border d-flex w-100 px-2">
+                            <div class="pt-2">
+                                <p><i class="fa fa-cc-visa text-primary pr-2"></i>Visa Debit Card</p>
+                            </div>
+                            <div class="ml-auto pt-2">************3456</div>
+                        </div>
+                    </div>
+                    
+                    <div class="d-flex flex-row w-100">
+                        <div class="pt-2 pr-2"><input type="radio" name="radio1" id="r2"></div>
+                        <div class="rounded d-flex w-100 px-2">
+                            <div class="pt-2">
+                                <p><i class="fa fa-cc-mastercard pr-2"></i>Mastercard Office</p>
+                            </div>
+                            <div class="ml-auto pt-2">************1038</div>
+                        </div>
+                    </div>
+                </form>
+                <div> <input type="button" value="Proceed to payment" class="btn btn-primary btn-block"> </div>
+            </div>
+        </div>
+        <div class="col-sm-3 col-md-4 offset-md-1 mobile">
+            <div class="py-4 d-flex justify-content-end">
+                <h6><a href="index.html?#plans">Cancel and return to website</a></h6>
+            </div>
+            <div class="bg-light rounded d-flex flex-column">
+                <div class="p-2 ml-3">
+                    <h4>Billing Info</h4>
+                </div>
+                <div class="p-2 d-flex">
+                    <div class="col-8">Name</div>
+                    <div class="ml-auto"> John Wick</div>
+                </div>
+                <div class="p-2 d-flex">
+                    <div class="col-8">Email</div>
+                    <div class="ml-auto"> jon@gmail.com</div>
+                </div>
+                <div class="p-2 d-flex">
+                    <div class="col-8">Address</div>
+                    <div class="ml-auto"> Any Street</div>
+                </div>
+                <div class="p-2 d-flex">
+                    <div class="col-8">City</div>
+                    <div class="ml-auto"> Orlando</div>
+                </div>
+                <div class="p-2 d-flex">
+                    <div class="col-8">State</div>
+                    <div class="ml-auto"> FL</div>
+                </div>
+                <div class="p-2 ml-3">
+                    <h4>Order Recap</h4>
+                </div>
+                
+                <div class="p-2 d-flex">
+                    <div class="col-8">Contracted Price</div>
+                    <div class="ml-auto"> $399</div>
+                </div>
+                <div class="p-2 d-flex">
+                    <div class="col-8">Tax</div>
+                    <div class="ml-auto">$0.00</div>
+                </div>
+                
+                
+                <div class="border-top px-4 mx-3"></div>
+                <div class="p-2 d-flex pt-3">
+                    <div class="col-8"><b>Total</b></div>
+                    <div class="ml-auto"><b class="green"> $399</b></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+                            </body>
+                        </html>
